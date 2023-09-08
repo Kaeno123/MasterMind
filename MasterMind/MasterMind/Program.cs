7c9364b64ca724d557a9c976cc865fe183a2fa6b
Kaeno Eyer <pg43msk@eduvaud.ch>
2023-09-08 15:34:39 +0200

    Version du 8 septembre 2023

--- MasterMind/MasterMind/Program.cs
+++ MasterMind/MasterMind/Program.cs
@@ -8,8 +8,78 @@ namespace MasterMind
 {
     internal class Program
     {
+        /// <summary>
+        /// Création du jeu MasterMind sur la console
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            byte numberOfCoulour = 6;
+            int numberOfChances = 10;
+            string essai1;
+            string ok;
+            string MaPo;
+
+            //texte de Bienvenue et introduction au jeu
+            Console.WriteLine("Bienvenue sur MasterMind !");
+            Console.WriteLine("Couleurs possibles: GYWRBMC");
+            Console.WriteLine("Devine le code en 4 couleurs.");
+
+            //
+            Console.Write("Essai 1: ");
+            essai1 = Console.ReadLine();
+            Console.WriteLine("=>Ok: ");
+            Console.WriteLine("Mauvaise position: ");
+
+            Console.Write("Essai 2: ");
+            essai1 = Console.ReadLine();
+            Console.WriteLine("=>Ok: ");
+            Console.WriteLine("Mauvaise position: ");
+
+            Console.Write("Essai 3: ");
+            essai1 = Console.ReadLine();
+            Console.WriteLine("=>Ok: ");
+            Console.WriteLine("Mauvaise position: ");
+
+            Console.Write("Essai 4: ");
+            essai1 = Console.ReadLine();
+            Console.WriteLine("=>Ok: ");
+            Console.WriteLine("Mauvaise position: ");
+
+            Console.Write("Essai 5: ");
+            essai1 = Console.ReadLine();
+            Console.WriteLine("=>Ok: ");
+            Console.WriteLine("Mauvaise position: ");
+
+            Console.Write("Essai 6: ");
+            essai1 = Console.ReadLine();
+            Console.WriteLine("=>Ok: ");
+            Console.WriteLine("Mauvaise position: ");
+
+            Console.Write("Essai 7: ");
+            essai1 = Console.ReadLine();
+            Console.WriteLine("=>Ok: ");
+            Console.WriteLine("Mauvaise position: ");
+
+            Console.Write("Essai 8: ");
+            essai1 = Console.ReadLine();
+            Console.WriteLine("=>Ok: ");
+            Console.WriteLine("Mauvaise position: ");
+
+            Console.Write("Essai 9: ");
+            essai1 = Console.ReadLine();
+            Console.WriteLine("=>Ok: ");
+            Console.WriteLine("Mauvaise position: ");
+
+            Console.Write("Essai 10: ");
+            essai1 = Console.ReadLine();
+            
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
