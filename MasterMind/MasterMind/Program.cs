7e16c2664f323da2cea5158cf203a4eef4dff2fe
Kaeno Eyer <pg43msk@eduvaud.ch>
2023-09-29 15:37:15 +0200

    version du 29.09.2023

--- MasterMind/MasterMind/Program.cs
+++ MasterMind/MasterMind/Program.cs
@@ -15,65 +15,79 @@ namespace MasterMind
         static void Main(string[] args)
         {
 
-            List<char> list = new List<char>();
-
-           /* string goal = "grws";*/
+            string[] goal = new string[4];
             string essai = "";
             string Ok;
             string MP;
             string reTry = "oui";
             int win;
-
-
-              Random random = new Random();
-            int x1 = random.Next(6);
-            int x2 = random.Next(6);
-            int x3 = random.Next(6);
-            int x4 = random.Next(6);
-
-
-            switch (x1)
-            {
-                case 0:
-                    
-                    break;
-            }
-            switch (x2)
-            {
-                case 0:
-                    break;
-            }
-            switch (x3)
-            {
-                case 0:
-                    break;
-            }
-            switch (x4)
-            {
-                case 0:
-                    break;
-            }
-
+            
 
             while (reTry == "oui")
             {
+                Random random = new Random();
+
+                int[] randomColors = new int[4];
+                randomColors[0] = random.Next(6);
+                randomColors[1] = random.Next(6);
+                randomColors[2] = random.Next(6);
+                randomColors[3] = random.Next(6);
+
+                for (int i = 0; i < randomColors.Length; i++)
+                {
+                    if (randomColors[i] == 0)
+                    {
+                        goal[i] = "g";
+                    }
+                    else if (randomColors[i] == 1)
+                    {
+                        goal[i] = "y";
+                    }
+                    else if (randomColors[i] == 2)
+                    {
+                        goal[i] = "w";
+                    }
+                    else if (randomColors[i] == 3)
+                    {
+                        goal[i] = "r";
+                    }
+                    else if (randomColors[i] == 4)
+                    {
+                        goal[i] = "m";
+                    }
+                    else if (randomColors[i] == 5)
+                    {
+                        goal[i] = "b";
+                    }
+                    else if (randomColors[i] == 6)
+                    {
+                        goal[i] = "c";
+                    }
+                }
+
+                string finalgoal = goal[0] + goal[1] + goal[2] + goal[3];
+
                 essai = "";
                 win = 0;
-                //texte de Bienvenue et introduction au jeu
-                Console.WriteLine("\nBienvenue sur MasterMind !");
+
+                Console.WriteLine(goal[0] + goal[1] + goal[2] + goal[3]);
+
+                //welcome text and introdocution to the game
+                Console.WriteLine("\n*******************************\nBienvenue sur MasterMind !");
                 Console.WriteLine("Couleurs possibles: gywrmbc");
-                Console.WriteLine("Devine le code en 4 couleurs.\n");
+                Console.WriteLine("Devine le code en 4 couleurs.\n*******************************\n");
+
 
                 for (int numberTry = 1; numberTry <= 11 && win == 0; numberTry++)
                 {
-                    //Si l'utilisateur gagne
-                    if (essai == goal)
+                    //if the user win
+                    if (essai == finalgoal)
                     {
                         numberTry = numberTry - 1;
                         Console.WriteLine("\nBravo, vous avez Gagnez en " + numberTry + " essais !! :)");
                         Console.Write("voulez-vous refaire ?\nMettez oui ou non: ");
-                        
-                        
+
+
                         reTry = Console.ReadLine();
 
                         while (reTry != "oui" && reTry != "non")
@@ -81,30 +95,27 @@ namespace MasterMind
 
                             Console.WriteLine("\nVous n'avez pas écrit les mots attendus\nVeuillez écrire oui ou non: ");
                             reTry = Console.ReadLine();
-                         
                         }
                         win = 1;
 
                     }
 
-                    //Affiche ce code si l'utilisateur perd après 10 essais
-                    else if (numberTry == 11 && essai != goal)
+                    //shows this code if the user loose after 10 try
+                    else if (numberTry == 11 && essai != finalgoal)
                     {
-                        Console.WriteLine("\nDommage, vous avez perdu :/");
+                        Console.WriteLine("\nDommage, vous avez perdu :/. Le code était ");
                         Console.Write("voulez-vous réessayer ?\nMettez oui ou non: ");
                         reTry = Console.ReadLine();
 
                         while (reTry != "oui" && reTry != "non")
                         {
-                            
-                                Console.WriteLine("\nVous n'avez pas écrit les mots attendus\nVeuillez écrire oui ou non: ");
-                                reTry = Console.ReadLine();
-                            
+                            Console.WriteLine("\nVous n'avez pas écrit les mots attendus\nVeuillez écrire oui ou non: ");
+                            reTry = Console.ReadLine();
                         }
 
                     }
 
-                    //éxécute ce code tant que l'utilisateur ne trouve pas le code ou fait moins de 10 essais
+                    //do this code while the user doesn't find the goal or do less than 10 try
                     else
                     {
                         Console.Write("\nEssai " + numberTry + " : ");
@@ -118,6 +129,4 @@ namespace MasterMind
 
         }
     }
-}
-
- 
+} 
